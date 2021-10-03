c158704bca1ab08bc36e6af8bf19aa55b01ef036
BolverBlitz <bolverblitz@ebg.pw>
2021-10-03 16:55:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ const db = new Influxdb({
 
 async function writeNewDataPoint() {
 	const Inv_Realtime_res = await fetch(`http://${process.env.Fronius_IP}/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System`);
+	const Inv_Realtime_V_res = await fetch(`http://${process.env.Fronius_IP}/solar_api/v1/GetInverterRealtimeData.cgi?scope=Device&DeviceId=1&DataCollection=3PInverterData`);
+	const Inv_Realtime_DC_res = await fetch(`http://${process.env.Fronius_IP}/solar_api/v1/GetInverterRealtimeData.cgi?scope=Device&DeviceId=1&DataCollection=CommonInverterData`);
+	const Inv_Day_Peaks_res = await fetch(`http://${process.env.Fronius_IP}/solar_api/v1/GetInverterRealtimeData.cgi?scope=Device&DeviceId=1&DataCollection=MinMaxInverterData`);
 	const Inv_Realtime_data = await Inv_Realtime_res.json();
+	const Inv_Realtime_V_data = await Inv_Realtime_V_res.json();
+	const Inv_Realtime_DC_data = await Inv_Realtime_DC_res.json();
+	const Inv_Day_Peaks_data = await Inv_Day_Peaks_res.json();
 	await db.write(
 	{
 		org: process.env.Database_Orga,
@@ -32,7 +38,21 @@ async function writeNewDataPoint() {
 				power: Inv_Realtime_data.Body.Data.PAC.Values[1],
                 wh_day: Inv_Realtime_data.Body.Data.DAY_ENERGY.Values[1],
                 wh_year: Inv_Realtime_data.Body.Data.YEAR_ENERGY.Values[1],
-                wh_total: Inv_Realtime_data.Body.Data.TOTAL_ENERGY.Values[1]
+                wh_total: Inv_Realtime_data.Body.Data.TOTAL_ENERGY.Values[1],
+				I_AC_L1: Inv_Realtime_V_data.Body.Data.IAC_L1.Value || 0,
+				I_AC_L2: Inv_Realtime_V_data.Body.Data.IAC_L2.Value || 0,
+				I_AC_L3: Inv_Realtime_V_data.Body.Data.IAC_L3.Value || 0,
+				U_AC_L1: Inv_Realtime_V_data.Body.Data.UAC_L1.Value || 0,
+				U_AC_L2: Inv_Realtime_V_data.Body.Data.UAC_L2.Value || 0,
+				U_AC_L3: Inv_Realtime_V_data.Body.Data.UAC_L3.Value || 0,
+				F_AC_All: Inv_Realtime_DC_data.Body.Data.FAC.Value || 0,
+				I_AC_All: Inv_Realtime_DC_data.Body.Data.IAC.Value || 0,
+				I_DC_All: Inv_Realtime_DC_data.Body.Data.IDC.Value || 0,
+				U_AC_All: Inv_Realtime_DC_data.Body.Data.UAC.Value || 0,
+				U_DC_All: Inv_Realtime_DC_data.Body.Data.UDC.Value || 0,
+				Peak_Day_P_AC: Inv_Day_Peaks_data.Body.Data.DAY_PMAX.Value || 0,
+				Peak_Day_V_AC: Inv_Day_Peaks_data.Body.Data.DAY_UACMAX.Value || 0,
+				Peak_Day_V_DC: Inv_Day_Peaks_data.Body.Data.DAY_UDCMAX.Value || 0
 			},
 	}]
 	);
